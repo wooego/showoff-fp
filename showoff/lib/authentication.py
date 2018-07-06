9045e60419552c91cb405b5093372d2207348e42
fp862 <fp862@163.com>
2018-07-06 17:13:18 +0800

    解决加密解密中使用bcrypt的hashpw函数未对加密内容进行编码的bug

--- showoff/lib/authentication.py
+++ showoff/lib/authentication.py
@@ -1,11 +1,11 @@
+#coding:utf-8
 import os, bcrypt
-
 
 def hash_password(seed, plaintext):
     """Encrypt given password using seed as an extra salt"""
-    return bcrypt.hashpw('%s%s' % (seed, plaintext), bcrypt.gensalt())
+    return bcrypt.hashpw(('%s%s' % (seed, plaintext)).encode('utf-8'), bcrypt.gensalt())
 
 
 def validate_password(seed, plaintext, hashed):
     """Validation function for blowfish encrypted password"""
-    return bcrypt.hashpw('%s%s' % (seed, plaintext), hashed) == hashed
+    return bcrypt.hashpw(('%s%s' % (seed, plaintext)).encode('utf-8'), hashed.encode('utf-8')).decode('utf-8') == hashed
